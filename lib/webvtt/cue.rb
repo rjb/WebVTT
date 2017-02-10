dcb9669ba2006f6f8ed3f7e2e4417c27900ac02b
Raffy Banks <raffybanks@gmail.com>
2017-02-09 21:09:27 -0800

    Added note

--- lib/webvtt/cue.rb
+++ lib/webvtt/cue.rb
@@ -1,11 +1,12 @@
 module WebVTT
   class Cue
-    attr_reader :start, :stop, :text
+    attr_reader :start, :stop, :text, :note
 
-    def initialize(start, stop, text)
+    def initialize(start, stop, text, note)
       @start = start
       @stop = stop
       @text = text
+      @note = note
     end
   end
 end
