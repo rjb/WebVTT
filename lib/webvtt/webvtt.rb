0b0e9d9f406dd4ec02072b08be2e40d06b11f21d
Raffy Banks <raffybanks@gmail.com>
2017-02-11 09:23:45 -0800

    Updated for changes to Cue

--- lib/webvtt/webvtt.rb
+++ lib/webvtt/webvtt.rb
@@ -47,80 +47,16 @@ module WebVTT
       text = @scanner.scan(/.*/)
 
       if @scanner.skip(/[\s]{1}/) && @scanner.peek(1) != "\n"
-        return text + ' ' + parse_text
+        return text + ' ' + parse_style
       else
         return text
       end
     end
 
     def parse_cues
-      loop do
-        cue_args = parse_cue
-        break unless cue_args
-        cues << WebVTT::Cue.new(cue_args)
-      end
-    end
-
-    def parse_cue
-      note = parse_note
-      identifier = parse_identifier
-      start, stop = parse_timestamp
-      text = parse_text
-
-      return false unless start && stop && text
-
-      {
-        start: start,
-        stop: stop,
-        text: text,
-        note: note,
-        identifier: identifier
-      }
-    end
-
-    def note_exist?
-      @scanner.check(/\s+NOTE\s+/)
-    end
-
-    def identifier_exist?
-      !@scanner.check(/\s+[0-9:.]+ --> [0-9:.]+/)
-    end
-
-    def parse_note
-      if note_exist?
-        @scanner.skip(/\s+NOTE\s+/)
-        parse_text
-      end
-    end
-
-    def parse_identifier
-      if identifier_exist?
-        @scanner.skip(/\s+/)
-        @scanner.scan(/.*/)
-      end
-    end
-
-    def parse_timestamp
-      @scanner.skip(/\s+/)
-
-      timestamp = @scanner.scan(/^[0-9:.]+/)
-
-      if @scanner.skip(/ --> /)
-        return [timestamp, parse_timestamp]
-      else
-        return timestamp
-      end
-    end
-
-    def parse_text
-      @scanner.skip(/[\s]{1}/)
-
-      text = @scanner.scan(/.*/)
-
-      if @scanner.skip(/[\s]{1}/) && @scanner.peek(1) != "\n"
-        return text + ' ' + parse_text
-      else
-        return text
+      @scanner.rest.split("\n\n").each do |chunk|
+        cue = Cue.parse(chunk)
+        cues << cue
       end
     end
   end
