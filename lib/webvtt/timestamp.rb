a15ba8e203dcc190b847f6d77c6007250aa9edac
Raffy Banks <raffybanks@gmail.com>
2017-02-11 15:53:33 -0800

    Added Timestamp class

---
+++ lib/webvtt/timestamp.rb
@@ -0,0 +1,44 @@
+module WebVTT
+  class Timestamp
+    attr_reader :hours
+    attr_reader :minutes
+    attr_reader :seconds
+    attr_reader :milliseconds
+
+    def self.parse(data)
+      timestamp = Timestamp.new(data)
+      timestamp.parse
+      timestamp
+    end
+
+    def initialize(data)
+      @data = data
+    end
+
+    def parse
+      @hours, @minutes, @seconds, @milliseconds = parse_timestamp
+    end
+
+    def to_s
+      hours + ':' + minutes + ':' + seconds + '.' + milliseconds
+    end
+
+    def join(other)
+      to_s + ' --> ' + other.to_s
+    end
+
+    private
+
+    def parse_timestamp
+      stamp = @data.match(/([0-9]{0,2}):{0,1}([0-9]{2}):([0-9]{2}).([0-9]{3})/)
+
+      if !stamp
+        raise 'Invalid timestamp.'
+      end
+
+      hours = stamp[1].empty? ? '00' : stamp[1]
+
+      [hours, stamp[2], stamp[3], stamp[4]]
+    end
+  end
+end
