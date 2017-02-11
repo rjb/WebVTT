983fc0647d5c63ce8991202c56d5138c2f8b9206
Raffy Banks <raffybanks@gmail.com>
2017-02-11 01:59:32 -0800

    Refactored

--- lib/webvtt/cue.rb
+++ lib/webvtt/cue.rb
@@ -2,12 +2,12 @@ module WebVTT
   class Cue
     attr_reader :start, :stop, :text, :note, :identifier
 
-    def initialize(start, stop, text, note, identifier)
-      @start = start
-      @stop = stop
-      @text = text
-      @note = note
-      @identifier = identifier
+    def initialize(args = {})
+      @start = args[:start] || nil
+      @stop = args[:stop] || nil
+      @text = args[:text] || nil
+      @note = args[:note] || nil
+      @identifier = args[:identifier] || nil
     end
 
     def timestamp
