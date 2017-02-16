d7299d27288071fd2596925333fb1e473edd49c4
Raffy Banks <raffybanks@gmail.com>
2017-02-15 23:32:16 -0800

    Added

--- test/webvtt_test.rb
+++ test/webvtt_test.rb
@@ -3,27 +3,41 @@ require 'test_helper'
 class WebVTTTest < Minitest::Test
   def setup
     @webvtt = WebVTT::File.read('test/speech.vtt')
-    @cue = @webvtt.cues.first
-    @setting = @cue.settings
   end
 
   def test_that_it_has_a_version_number
     refute_nil ::WebVTT::VERSION
   end
 
-  def test_instance_of_cue_setting
-    assert_instance_of WebVTT::Setting, @setting
+  def test_cues_count
+    assert_equal 2, @webvtt.cues.count
   end
 
-  def test_setting_line
-    assert_equal '63%', @setting.line
+  def test_instances_of_cue
+    @webvtt.cues.each do |cue|
+      assert_instance_of WebVTT::Cue, cue
+    end
   end
 
-  def test_setting_position
-    assert_equal '72%', @setting.position
+  def test_comments_count
+    assert_equal 1, @webvtt.comments.count
   end
 
-  def test_setting_align
-    assert_equal 'start', @setting.align
+  def test_instances_of_note
+    @webvtt.comments.each do |comment|
+      assert_instance_of WebVTT::Comment, comment
+    end
+  end
+
+  def test_instance_of_region
+    assert_instance_of WebVTT::Region, @webvtt.region
+  end
+
+  def test_style
+    style = 
+      '::cue(v[voice="Esme"]) { color: cyan } ' \
+      '::cue(v[voice="Mary"]) { color: lime } ' \
+      '::cue(i) { font-style: italic }'
+    assert_equal style, @webvtt.style
   end
 end
